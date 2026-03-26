f108551b4a5983775e484beb095502ac2724f5bf
Sergey Pribylskiy <asp@Mac.lan>
2026-03-26 14:46:29 +0500

    Init

---
+++ downloads.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+wget -P ./result -O ./result/1.txt https://github.com/AvenCores/goida-vpn-configs/raw/refs/heads/main/githubmirror/1.txt
+wget -P ./result -O ./result/2.txt https://github.com/AvenCores/goida-vpn-configs/raw/refs/heads/main/githubmirror/2.txt
+wget -P ./result -O ./result/3.txt https://github.com/AvenCores/goida-vpn-configs/raw/refs/heads/main/githubmirror/3.txt
+wget -P ./result -O ./result/4.txt https://github.com/AvenCores/goida-vpn-configs/raw/refs/heads/main/githubmirror/4.txt
+wget -P ./result -O ./result/5.txt https://github.com/AvenCores/goida-vpn-configs/raw/refs/heads/main/githubmirror/5.txt
+wget -P ./result -O ./result/6.txt https://github.com/AvenCores/goida-vpn-configs/raw/refs/heads/main/githubmirror/6.txt
+wget -P ./result -O ./result/7.txt https://github.com/AvenCores/goida-vpn-configs/raw/refs/heads/main/githubmirror/7.txt
+wget -P ./result -O ./result/8.txt https://github.com/AvenCores/goida-vpn-configs/raw/refs/heads/main/githubmirror/8.txt
+wget -P ./result -O ./result/9.txt https://github.com/AvenCores/goida-vpn-configs/raw/refs/heads/main/githubmirror/9.txt
+
+wget -P ./result -O ./result/26.txt https://github.com/AvenCores/goida-vpn-configs/raw/refs/heads/main/githubmirror/26.txt
+wget -P ./result -O ./result/25.txt https://github.com/AvenCores/goida-vpn-configs/raw/refs/heads/main/githubmirror/25.txt
+
+cat ./result/* | grep xhttp > xhttp.txt
+
+
+
